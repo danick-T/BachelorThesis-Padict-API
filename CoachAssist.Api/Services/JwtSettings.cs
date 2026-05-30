841c1c14af034835859096780fcd4a409d8e7bb6
danick-T <danick.tchang@student.odisee.be>
2026-05-30 04:03:29 +0200

    update

--- CoachAssist.Api/Services/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace CoachAssist.Api.Services;
-
-public class JwtSettings
-{
-    public string SecretKey { get; set; } = string.Empty;
-    public string Issuer { get; set; } = string.Empty;
-    public string Audience { get; set; } = string.Empty;
-    public int ExpiryInDays { get; set; } = 7;
-}
